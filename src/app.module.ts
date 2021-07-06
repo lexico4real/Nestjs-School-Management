e3acf0aa826708390680a667ced5dde84c61186c
lexico4real <lexico4real@gmail.com>
2021-07-06 11:51:41 +0100

    Added teachers service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeachersController } from './teachers/teachers.controller';
+import { TeachersService } from './teachers/teachers.service';
+import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TeachersModule],
+  controllers: [AppController, TeachersController],
+  providers: [AppService, TeachersService],
 })
 export class AppModule {}
